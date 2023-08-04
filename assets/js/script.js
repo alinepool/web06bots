fe192d6cd9d55c8963f180b2002bddaf97ca76e0
alinepool <140113064+alinepool@users.noreply.github.com>
2023-08-04 00:15:57 -0600

    Pagina completa

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ console.log("Pagina en contrucción con Patricio Estrella");
 function clickPatricio(){
     alert("La mayonesa es un instrumento?");
 }
+
+function changeColor( elementHtml, color ) {
+    // alert("Que emoción, voy a cambiar mi color")
+    console.log(elementHtml);
+    console.log(color);
+    
+    elementHtml.style.color = color;
+}
+
+function changeColorWithPromt( elementHtml) {
+    const color = prompt("Dime el color en inglés", "yellow");
+    changeColor( elementHtml, color );
+}
