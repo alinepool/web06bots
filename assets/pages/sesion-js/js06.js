f57e409921019eeacf98541b6087811b1cd67370
alinepool <140113064+alinepool@users.noreply.github.com>
2023-08-15 13:54:19 -0600

    Js06Form

--- assets/pages/sesion-js/js06.js
+++ assets/pages/sesion-js/js06.js
@@ -9,7 +9,7 @@ const findElementById = () => {
     console.log( typeof title);
     console.log(title.innerHTML);
     //title.innerHTML = `Generation - Sesión JS 06`;
-    title.innerHTML = `<span class="text-primary"> Generation </span> - Sesión JS 06`;
+    title.innerHTML = `<span id="generetion" class="text-primary"> Generation </span> - Sesión JS 06`;
 }
 
 findElementById();
@@ -56,7 +56,43 @@ const newElement = () => {
     </ul>
     `;
 
-    const descriptionch30 = document.querySelector("descriptionch30");
+    const descriptionch30 = document.querySelector("#descriptionch30");
     descriptionch30.appendChild(newElement);
 }
 newElement();
+
+//-------------------Cambiar el color de texto------------------
+//style.color
+const changeColor = (color ) => {
+    const descriptionch30 = document.getElementById("descriptionch30");
+    descriptionch30.style.color = color;
+    descriptionch30.style.border = `thin solid ${color}`;
+}
+
+changeColor( "beige" );
+//------------------------Propiedades de visualizacion---------
+//                 Desaparecer el elemento
+// display : none (quitar el elemento)
+//visibility hidden (ocultar el elemento)
+
+const getReferenceTitleGeneration = () => {
+    return document.getElementById("generation");
+}
+
+
+const displayNoneElement = () => {
+    const generetion = document.getElementById("generetion");
+    generation.tyle.display = "none";//quitar elemento
+
+}
+
+const hiddenElement = () => {
+    const generetion = document.getElementById("generetion");
+    generation.tyle.visibility = "hiden";//quitar elemento
+}
+
+const resetElements = () => {
+    const generetion = document.getElementById("generetion");
+    generation.tyle.visibility = "visible";//quitar elemento
+    generation.tyle.display = "inline";//quitar elemento
+}
